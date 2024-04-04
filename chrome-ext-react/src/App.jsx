59949036731ff582e7fcaeec2ae46e69ac5a8aa4
Rayyan Shaik <56854894+rayyanshaik2022@users.noreply.github.com>
2024-04-04 14:37:48 -0400

    minor edits

--- chrome-ext-react/src/App.jsx
+++ chrome-ext-react/src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box, Button, Flex, useMantineColorScheme, Title} from '@mantine/core'
 
+// Contains the the extension and its sizing
 function App() {
-  const [count, setCount] = useState(0)
+
+  useMantineColorScheme("dark")
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Box w={"280px"} bg={"dark"}>
+      <Flex direction={"column"} gap={"48px"} align={"center"} p={"32px"}>
+        <Title order={1} size={"h1"} c={"orange"}>
+          ProfitPeak
+        </Title>
+        <Button color={"orange"} radius={"md"} size="md">Connect Wallet</Button>
+      </Flex>
+    </Box>
   )
 }
 
--- chrome-ext-react/src/main.jsx
+++ chrome-ext-react/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import "@mantine/core/styles.css";
+
+import { MantineProvider } from "@mantine/core";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <MantineProvider>
+            <App />
+        </MantineProvider>
+    </React.StrictMode>
+);
