1018d3742ac9e42187fcb84a536c86cae1fb397b
aabdel0181 <ahmedabdellahhoco@gmail.com>
2024-04-09 22:28:32 +0200

    getting basic info

--- chrome-ext-react/data.js
+++ chrome-ext-react/data.js
@@ -1,7 +1,8 @@
 // Define the API URL
 async function async_get_txs(api_key, address, start_date) {
-  const norm_txs = `https://api-sepolia.arbiscan.io/api?module=account&action=txlist&address=${address}&startblock=${start_date}&endblock=99999999&page=1&offset=10&sort=asc&apikey=${api_key}`;
-
+  // NO LONGER ON ARBITRUM
+  // const norm_txs = `https://api-sepolia.arbiscan.io/api?module=account&action=txlist&address=${address}&startblock=${start_date}&endblock=99999999&page=1&offset=10&sort=asc&apikey=${api_key}`;
+  const norm_txs = `https://api-sepolia.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=${start_date}&endblock=99999999&page=1&offset=10&sort=asc&apikey=${api_key}`;
   try {
     const response = await fetch(norm_txs);
     if (!response.ok) {
@@ -16,23 +17,22 @@ async function async_get_txs(api_key, address, start_date) {
 }
 
 (async () => {
-  // An IIFE (Immediately Invoked Function Expression) to use await at the top level
   const tx_response = await async_get_txs(
-    "BPEKEDICIBHURQCA92GMSMI8RFGYEFHGDV",
-    "0xd262677A79Fb6962084a546D63a162F2336de298",
+    "ZSPYYMRN3UBZTQ1KF3VMHY19JUKNRYWTF5",
+    "0xbE197f43EC7b1B0f50BACF77c12C262C435EeD4D",
     "0"
   );
 
-  // Processing the response:
+  // Processing the response
   if (
     tx_response &&
     tx_response.result &&
     tx_response.result.some(
       (tx) =>
-        tx.to.toLowerCase() ===
-        "0xd262677a79fb6962084a546d63a162f2336de298".toLowerCase()
+        tx.from.toLowerCase() ===
+        "0xbE197f43EC7b1B0f50BACF77c12C262C435EeD4D".toLowerCase()
     )
   ) {
-    console.log("Wow");
+    console.log("Found");
   }
 })();
